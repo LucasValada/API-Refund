ba0671cae4bf405caa13e9442b99e4a6567ec02c
Lucas Valadão <valadaolucas6@gmail.com>
2026-01-29 19:20:44 -0300

    feat: install new packages and treat exceptions with zod

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express from "express";
-
+import cors from "cors";
+import { errorHandling } from "./middlewares/error-handling";
+import z from "zod";
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
+
 app.get("/", (req, res) => {
+  const bodySchema = z.object({ age: z.string() });
+  const { age } = bodySchema.parse(req.body);
   res.send("Hello World");
 });
+
+app.use(errorHandling);
+
 export default app;
